ba011b8febebb6428cb1a772abeb42f7fe8e51bd
Matt <mateusz.worek@gmail.com>
2021-09-28 09:04:51 +0200

    Add files via upload

---
+++ src/components/BreadCrumb/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+// PropTypes
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+// Styles
+import { Wrapper, Content } from './BreadCrumb.styles'
+
+const BreadCrumb = ({ movieTitle }) => {
+  return (
+    <>
+      <Wrapper>
+        <Content>
+          <Link to='/'>
+            <span>Home</span>
+          </Link>
+          <span>|</span>
+          <span>{movieTitle}</span>
+        </Content>
+      </Wrapper>
+    </>
+  )
+}
+
+BreadCrumb.propTypes = {
+  movieTitle: PropTypes.string,
+}
+
+export default BreadCrumb
